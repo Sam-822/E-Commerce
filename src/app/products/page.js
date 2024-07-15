895e239952fca9b6057b96d50ebf6dcfeaf35c37
AbdulSamad <ansamad1028@gmail.com>
2024-07-15 23:17:04 +0530

    fixed mistake

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,8 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import Categories from "../component/Categories";
 
 const page = () => {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
 
   return (
     <div>
