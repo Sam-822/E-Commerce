b48fd63a7e503dbb6cbd859c5e0539363695ef05
AbdulSamad <ansamad1028@gmail.com>
2024-07-30 16:46:00 +0530

    added my footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from "./component/Footer";
+import MyFooter from "./component/MyFooter";
 
 export const metadata = {
   title: "ShopBuddy - Your favorite e-commerce website",
@@ -17,12 +18,13 @@ export default function RootLayout({ children }) {
           <NavBar />
         </header>
         {children}
-        <footer className="mb-0" style={{marginTop:200}}>
+        <footer className="mb-0" style={{ marginTop: 200 }}>
           <Footer />
           <p className="text-center vw-100 fs-5 mt-5">
             ----- Made with <span className="heart"> ❤️</span> by Abdul Samad
             Ansari -----
           </p>
+          <MyFooter />
         </footer>
       </body>
     </html>
